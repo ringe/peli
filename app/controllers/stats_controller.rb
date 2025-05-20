cbc9915805fd6db2cc2dd11a788ba6e9dc49e023
Runar Ingebrigtsen <runar@rin.no>
2025-05-20 02:51:19 +0200

    GET /stats/impact_report

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,15 +2,30 @@ class StatsController < ApplicationController
   def weekly_summary
     players = Player.joins(:playthroughs)
                     .merge(Playthrough.weeks_ago(weeks))
-                    .group("accounts.id", "accounts.name")
-                    .select("accounts.id as id",
-                            "accounts.player_id as player_id",
-                            "accounts.name as name",
+                    .select(:id, :player_id, :name,
                             "TOTAL(playthroughs.time_spent) as total_duration",
                             "TOTAL(playthroughs.score) as total_score")
+                    .group("accounts.id")
                     .order(sort)
 
     render json: players.to_json
+  end
+
+  def impact_report
+    first_score = "(SELECT score FROM playthroughs p2 WHERE p2.player_id = accounts.id ORDER BY started_at ASC LIMIT 1)"
+    highest_score = "MAX(playthroughs.score)"
+
+    players = Player
+      .joins(:playthroughs)
+      .select(:id, :player_id, :name,
+        "#{first_score} AS first_score",
+        "#{highest_score} AS highest_score",
+        "(#{highest_score} - #{first_score}) AS impact"
+      )
+      .group("accounts.id")
+      .order("impact desc")
+
+      render json: players.to_json
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
 
   get "stats/weekly_summary" => "stats#weekly_summary"
+  get "stats/impact_report" => "stats#impact_report"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
